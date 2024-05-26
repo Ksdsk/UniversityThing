6160bc6594d60db1fb90d241e474ed052508949f
Daniel Kang <danielkang@dal.ca>
2024-05-25 22:56:13 -0300

    db hide

--- lambdas/reset-db/tests/unit_test.py
+++ lambdas/reset-db/tests/unit_test.py
@@ -1,85 +1,80 @@
-# RESET DB AAAAAAAAAA
+import json
+from scripts.app import lambda_handler
 
+# REPLACE THESE VALUES WITH YOUR OWN
+DUMMY_FIRST_NAME = "John"
+DUMMY_LAST_NAME = "Doe"
 
-# import json
-# from scripts.app import lambda_handler
+def test_returns_200():
+    # Prepare a sample event
+    event = {
+        "body": json.dumps({})
+    }
 
-# DUMMY_FIRST_NAME = "John"
-# DUMMY_LAST_NAME = "Doe"
+    # Call the lambda_handler function with the sample event
+    response = lambda_handler(event, None)
 
-# def test_returns_200():
-#     # Prepare a sample event
-#     event = {
-#         "body": json.dumps({
-#             "first_name": DUMMY_FIRST_NAME,
-#             "last_name": DUMMY_LAST_NAME
-#         })
-#     }
+    # Perform assertions on the response
+    assert response['statusCode'] == 200
+    assert json.loads(response['body'])['message'] == f"Hello {DUMMY_FIRST_NAME} {DUMMY_LAST_NAME}!"
 
-#     # Call the lambda_handler function with the sample event
-#     response = lambda_handler(event, None)
+def test_returns_400_because_no_event():
+    # Prepare a sample event
+    event = None
 
-#     # Perform assertions on the response
-#     assert response['statusCode'] == 200
-#     assert json.loads(response['body'])['message'] == f"Hello {DUMMY_FIRST_NAME} {DUMMY_LAST_NAME}!"
+    # Call the lambda_handler function with the sample event
+    response = lambda_handler(event, None)
 
-# def test_returns_400_because_no_event():
-#     # Prepare a sample event
-#     event = None
+    # Perform assertions on the response
+    assert response['statusCode'] == 400
+    assert json.loads(response['body'])['message'] == "No event"
 
-#     # Call the lambda_handler function with the sample event
-#     response = lambda_handler(event, None)
+def test_returns_400_because_no_event_body():
+    # Prepare a sample event
+    event1 = {
+        "body": None
+    }
 
-#     # Perform assertions on the response
-#     assert response['statusCode'] == 400
-#     assert json.loads(response['body'])['message'] == "No event"
+    event2 = {
+        "something": "else"
+    }
 
-# def test_returns_400_because_no_event_body():
-#     # Prepare a sample event
-#     event1 = {
-#         "body": None
-#     }
+    # Call the lambda_handler function with the sample event
+    response1 = lambda_handler(event1, None)
+    response2 = lambda_handler(event2, None)
 
-#     event2 = {
-#         "something": "else"
-#     }
+    # Perform assertions on the response
+    assert response1['statusCode'] == 400
+    assert json.loads(response1['body'])['message'] == "Missing event body"
+    assert response2['statusCode'] == 400
+    assert json.loads(response2['body'])['message'] == "Missing event body"
 
-#     # Call the lambda_handler function with the sample event
-#     response1 = lambda_handler(event1, None)
-#     response2 = lambda_handler(event2, None)
+def test_returns_400_because_no_last_name():
+    # Prepare a sample event
+    event = {
+        "body": json.dumps({
+            "first_name": DUMMY_FIRST_NAME
+        })
+    }
 
-#     # Perform assertions on the response
-#     assert response1['statusCode'] == 400
-#     assert json.loads(response1['body'])['message'] == "Missing event body"
-#     assert response2['statusCode'] == 400
-#     assert json.loads(response2['body'])['message'] == "Missing event body"
+    # Call the lambda_handler function with the sample event
+    response = lambda_handler(event, None)
 
-# def test_returns_400_because_no_last_name():
-#     # Prepare a sample event
-#     event = {
-#         "body": json.dumps({
-#             "first_name": DUMMY_FIRST_NAME
-#         })
-#     }
+    # Perform assertions on the response
+    assert response['statusCode'] == 400
+    assert json.loads(response['body'])['message'] == "Missing last_name"
 
-#     # Call the lambda_handler function with the sample event
-#     response = lambda_handler(event, None)
+def test_returns_400_because_no_first_name():
+    # Prepare a sample event
+    event = {
+        "body": json.dumps({
+            "last_name": DUMMY_LAST_NAME
+        })
+    }
 
-#     # Perform assertions on the response
-#     assert response['statusCode'] == 400
-#     assert json.loads(response['body'])['message'] == "Missing last_name"
+    # Call the lambda_handler function with the sample event
+    response = lambda_handler(event, None)
 
-# def test_returns_400_because_no_first_name():
-#     # Prepare a sample event
-#     event = {
-#         "body": json.dumps({
-#             "last_name": DUMMY_LAST_NAME
-#         })
-#     }
-
-#     # Call the lambda_handler function with the sample event
-#     response = lambda_handler(event, None)
-
-#     # Perform assertions on the response
-#     assert response['statusCode'] == 400
-#     assert json.loads(response['body'])['message'] == "Missing first_name"
+    # Perform assertions on the response
+    assert response['statusCode'] == 400
+    assert json.loads(response['body'])['message'] == "Missing first_name"
