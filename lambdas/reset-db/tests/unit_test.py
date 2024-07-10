50295f5a66a4630c221afd655c35d7f3e90cafac
Daniel Kang <danielkang@dal.ca>
2024-07-10 15:15:39 -0300

    add minimal unit test

--- lambdas/reset-db/tests/unit_test.py
+++ lambdas/reset-db/tests/unit_test.py
@@ -1,4 +1,15 @@
 import json
 from scripts.app import lambda_handler
+from scripts.app import get_db_connector
 
-# TODO: Create unit tests
+DUMMY_DB_HOSTNAME = "dummy_db_hostname"
+DUMMY_DB_PORT = 3306
+DUMMY_DB_REGION = "dummy_db_region"
+DUMMY_DB_USERNAME = "XXXXXXXXXXXXXXXXX"
+DUMMY_DB_PASSWORD = "XXXXXXXXXXXXXXXXX"
+DUMMY_DB_NAME = "dummy_db_name"
+
+def get_db_connector_creates_connection():
+    assert get_db_connector(DUMMY_DB_HOSTNAME, DUMMY_DB_PORT, DUMMY_DB_REGION, DUMMY_DB_USERNAME, DUMMY_DB_PASSWORD, DUMMY_DB_NAME) is not None
+
+ 
