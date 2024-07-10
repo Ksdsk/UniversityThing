622f84bbe9102d16b02a455b98d85b96ac934591
Daniel Kang <danielkang@dal.ca>
2024-07-10 15:27:45 -0300

    unit tests

--- lambdas/reset-db/tests/unit_test.py
+++ lambdas/reset-db/tests/unit_test.py
@@ -9,7 +9,7 @@ DUMMY_DB_USERNAME = "XXXXXXXXXXXXXXXXX"
 DUMMY_DB_PASSWORD = "XXXXXXXXXXXXXXXXX"
 DUMMY_DB_NAME = "dummy_db_name"
 
-def get_db_connector_creates_connection():
-    assert get_db_connector(DUMMY_DB_HOSTNAME, DUMMY_DB_PORT, DUMMY_DB_REGION, DUMMY_DB_USERNAME, DUMMY_DB_PASSWORD, DUMMY_DB_NAME) is not None
+# Most of these unit tests require mocking. I'll bombard those with you later, because they are very very boring to do imo.
 
- 
+# def test_get_db_connector_creates_connection():
+#     assert get_db_connector(DUMMY_DB_HOSTNAME, DUMMY_DB_PORT, DUMMY_DB_REGION, DUMMY_DB_USERNAME, DUMMY_DB_PASSWORD, DUMMY_DB_NAME) is not None
